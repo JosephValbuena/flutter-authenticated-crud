22a5bcaff855462176c3120af9af71a6450dee50
Joseph Valbuena <josvalk775@gmail.com>
2026-02-16 21:30:49 -0500

    creacion de metodo de verificacion de token y obtencion de token

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
+import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service_impl.dart';
 
+import '../../../shared/infrastructure/services/key_value_storage_service.dart';
 import '../../domain/domain.dart';
 
 enum AuthStatus {checking, authenticated, notAuthenticated}
@@ -8,19 +10,25 @@ enum AuthStatus {checking, authenticated, notAuthenticated}
 final authProvider = StateNotifierProvider.autoDispose<AuthNotifier, AuthState>((ref) {
 
   final authRepository = AuthRepositoryImpl();
+  final keyValueStorageService = KeyValueStorageServiceImpl();
 
   return AuthNotifier(
-    authRepository: authRepository
+    authRepository: authRepository,
+    keyValueStorageService: keyValueStorageService
   );
 });
 
 class AuthNotifier extends StateNotifier<AuthState> {
 
   final AuthRepository authRepository;
+  final KeyValueStorageService keyValueStorageService;
 
   AuthNotifier({
-    required this.authRepository
-  }): super(AuthState());
+    required this.authRepository,
+    required this.keyValueStorageService,
+  }): super(AuthState()) {
+    checkAuthStatus();
+  }
   
   Future<void> loginUser(String email, String password) async {
     await Future.delayed(const Duration(microseconds: 500));
@@ -29,9 +37,9 @@ class AuthNotifier extends StateNotifier<AuthState> {
       final user = await authRepository.login(email, password);
       _setLoggedUser(user);
     } on CustomError catch (e) {
-      logout(e.message);
+      logout(errorMessage: e.message);
     } catch (e) {
-      logout('Error no controlado');
+      logout(errorMessage: 'Error no controlado');
     }
   }
 
@@ -40,12 +48,20 @@ class AuthNotifier extends StateNotifier<AuthState> {
   }
 
   void checkAuthStatus() async {
+    final token = await keyValueStorageService.getValue<String>('token');
 
+    if(token == null) return logout();
+
+    try {
+      final user = await authRepository.checkAuthStatus(token);
+      _setLoggedUser(user);
+    } catch (e) {
+      
+    }
   }
 
-  void _setLoggedUser(User user) {
-    //TODO: necesito guardar el token en el dispositivo
-
+  void _setLoggedUser(User user) async {
+    await keyValueStorageService.setKeyValue('token', user.token);
     state = state.copyWith(
       user: user,
       authStatus: AuthStatus.authenticated,
@@ -53,8 +69,8 @@ class AuthNotifier extends StateNotifier<AuthState> {
     );
   }
 
-  Future<void> logout(String? errorMessage) async {
-    //TODO: Limpiar token
+  Future<void> logout({String? errorMessage}) async {
+    await keyValueStorageService.removeKey('token');
     state = state.copyWith(
       authStatus: AuthStatus.notAuthenticated,
       user: null,
